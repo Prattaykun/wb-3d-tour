d1fe233036d35ed93330675ac544756b5caeb4b8
Prattaykun <prattaystudent@gmail.com>
2025-08-16 20:55:36 +0530

    app and ios add

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.wbtour.app',
+  appName: ' WBtour',
+  webDir: 'out'
+};
+
+export default config;

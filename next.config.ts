1d100deaf143ccc2e84831e9a9ba04a9ac2c38d7
Prattaykun <prattaystudent@gmail.com>
2025-08-25 00:23:57 +0530

    deploy fix 3

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   transpilePackages: ["framer-motion"], // ✅ Fix for "export *" issue
-   ignoreDuringBuilds: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
